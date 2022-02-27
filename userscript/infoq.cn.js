43cc1533265b1b36dfd1017b3b94553a078b4593
jiacai2050 <dev@liujiacai.net>
2022-02-27 22:42:20 +0800

    add infoq userscript

---
+++ userscript/infoq.cn.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         InfoQ 纯净阅读
+// @namespace    https://github.com/jiacai2050
+// @version      0.1
+// @description  去除页面上无关内容，只为阅读!
+// @author       jiacai2050
+// @match        https://*infoq.cn/*
+// @icon         https://www.google.com/s2/favicons?domain_url=https://infoq.cn
+// @require      https://code.jquery.com/jquery-1.12.4.min.js
+// @grant        none
+// @license      MIT
+// ==/UserScript==
+
+(function() {
+  let selectorsToRemove = [
+    'div.geo-banner',
+    'div.header-notice',
+    'div.friendship-link-wrap',
+    'div.com-app-download',
+    'div.hot-ppt',
+    'div.widget-minibook'
+  ];
+  let cleanWorker = setInterval(function() {
+    let rets = [];
+
+    for(let sel of selectorsToRemove) {
+      rets.push($(sel).remove().length);
+    }
+
+    let sum = rets.reduce((a, b) => a+b, 0);
+    console.log(`removed ${sum}`);
+    if(sum === selectorsToRemove.length || sum === 0) { // all div are removed
+      clearInterval(cleanWorker);
+    }
+  }, 1000);
+
+})();
