fee52c4de8a0030fccd6fe42584ca62c1173921f
jiacai2050 <dev@liujiacai.net>
2022-07-30 11:21:38 +0800

    add userscript remove github unread

---
+++ userscript/github-remove-unread.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name        Remove GitHub unread notification
+// @namespace   https://github.com/jiacai2050
+// @match       https://github.com/
+// @grant       none
+// @version     0.1.0
+// @author      jiacai2050
+// @icon        https://github.githubassets.com/favicons/favicon.svg
+// @description Help programmers have a happy weekend
+// ==/UserScript==
+
+function isWeekend(date = new Date()) {
+  return date.getDay() === 6 || date.getDay() === 0;
+}
+
+function removeUnread() {
+  if (isWeekend) {
+    for(let unread of document.getElementsByClassName('unread')) {
+      unread.remove();
+    }
+  }
+}
+
+window.addEventListener('load', removeUnread);
